972e2edc0838ab1b4a5214e07bed939e677ccc5d
chysch <39314199+chysch@users.noreply.github.com>
2018-07-04 12:39:14 +0300

    Train without hyper-parameters

--- train.py
+++ train.py
@@ -1,8 +1,35 @@
 import sys
+from train_funcs import *
 
 def Train(output, rules, dicts):
     print("Training...")
-    #TODO: Implement...
+    
+    rule_file = open(rules, 'r')
+    rule_lines = rule_file.readlines()
+    rule_file.close()
+
+    rule_lines = PrepareRules(rule_lines)
+    
+    output_list = []
+    for d in dicts:
+        dict_file = open(d, 'r')
+        dict_lines = dict_file.readlines()
+        dict_file.close()
+
+        dict_lines = PrepareList(dict_lines)
+        
+        output_list = output_list + ConvertList(\
+            rule_lines, dict_lines)
+
+    (fore_lines, back_lines) = GetLists(output_list)
+    
+    fore_file = open(output + '.fore', 'w')
+    fore_file.writelines(fore_lines)
+    fore_file.close()
+
+    back_file = open(output + '.back', 'w')
+    back_file.writelines(back_lines)
+    back_file.close()
 
 
 if __name__ == '__main__':
---
+++ train_funcs.py
@@ -0,0 +1,57 @@
+import re
+
+def RuleReplace(rule, line):
+    res = line
+    # TODO: Implement...
+    return res
+
+def PrepareRules(rule_lines):
+    res = []
+    funcs = {"Replace":RuleReplace} # TODO: Add more...
+    for rule in rule_lines:
+        (d,i) = rule.split(':')
+        (f,p) = d.split(',')
+        (l,r) = i.split(',')
+        entry = []
+        entry.append(funcs[f])
+        entry.append(i)
+        entry.append(l.split(' '))
+        entry.append(r.split(' '))
+        res.append(tuple(entry))
+    return res
+
+def ApplyRules(rule_lines, line):
+    res = line
+    for rule in rule_lines:
+        res = rule[0](rule, res)
+    return res
+
+def ConvertList(rule_lines, dict_lines):
+    res = []
+    for line in dict_lines:
+        res.append(ApplyRules(rule_lines, line))
+    return res
+
+def PrepareList(dict_lines):
+    res = []
+    for line in dict_lines:
+        [w,d] = line.rstrip('\n').split('\t')
+        m = re.fullmatch(r"(.)*(\([0-9]\))",w)
+        if m:
+            w = w.rstrip(m.group(2))
+        d = d.split(' ')
+        entry = (w,d)
+        res.append(entry)
+    return res
+
+def GetLists(data):
+    res = ([],[])
+    for entry in data:
+        d = ' '
+        d = d.join(entry[1])
+        fore = entry[0] + '\t' + d + '\n'
+        back = d + '\t' + entry[0] + '\n'
+        res[0].append(fore)
+        res[1].append(back)
+    return res
+
