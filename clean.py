5b9304d039e09ecbb68009053c1cb215cd3f7f4d
chysch <39314199+chysch@users.noreply.github.com>
2018-09-12 22:46:26 +0300

    Update clean.py

--- clean.py
+++ clean.py
@@ -14,7 +14,9 @@ def Clean(output, raw, rules):
     rule_lines = rule_file.readlines()
     rule_file.close()
     sent_rules = PrepareRules(rule_lines)[2]
-    length_threshold = int(sent_rules['Threshold'])
+    length_threshold = 1000
+    if 'Threshold' in sent_rules:
+        length_threshold = int(sent_rules['Threshold'])
     online_mode = True if sent_rules['OnlineMode'].lower() == 'on' else False
 
     raw_file = open(raw, 'r')
